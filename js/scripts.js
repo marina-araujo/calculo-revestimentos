28f2d91534475ccfcad1b9d6d236433f7707aff3
Marina Barbosa Araujo <marina.ba@gmail.com>
2025-02-19 11:08:55 -0300

    finalizando cálculos principais

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,75 @@
+// constantes globais:
+
+
 // capturar evento de submit do formulario
 const form = document.querySelector('#formulario');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('evento previnido.');
-    setResultado('ola mundo');
+    const inputLargura = e.target.querySelector('#largura');
+    const inputAltura = e.target.querySelector('#altura');
+    const inputLarRev = e.target.querySelector('#larrev');
+    const inputAltRev = e.target.querySelector('#altrev');
+
+    const altPar = Number(inputAltura.value);
+    const largPar = Number(inputLargura.value);
+    const largRev = Number(inputLarRev.value);
+    const altRev = Number(inputAltRev.value);
+
+    if ((!altPar) || (!largPar)) {
+        setResultado('Informação inválida. Verifique se digitou algum número válido nos campos.', false);
+        return;
+    }
+
+    const areaPar = getAreaPar(altPar, largPar);
+    const areaRev = getAreaRev(altRev, largRev);
+    const qtdRev = getQtdRev(areaPar, areaRev);
+    const areaSobra = getAreSobra(areaPar);
+
+    const msg = `A quantidade de peças necessárias é de: ${qtdRev} e a área recomendada a se considerar para comprar (com 20% de sobra) é ${areaSobra}m².`;
+    setResultado(msg, true);
+
+    //console.log('evento previnido.');
+    //setResultado('ola mundo');
+
 });
 
-// função setresultado
-function setResultado (msg) {
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = `<p>${msg}</p>`;
+function getAreaPar(altPar, largPar) {
+    const areaPar = altPar * largPar;
+    return areaPar.toFixed(2);
+};
+
+function getAreaRev(altRev, largRev) {
+    const areaRev = altRev * largRev;
+    return areaRev.toFixed(2);
+};
+
+function getQtdRev(areaPar, areaRev) {
+    const qtdRev = areaPar / areaRev;
+    const qtdRevArredondado = Math.ceil(qtdRev);
+    return qtdRevArredondado;
+
+};
+
+function getAreSobra(areaPar) {
+    const areaSobra = areaPar * 1.2;
+    return areaSobra.toFixed(2);
 }
+
+function criaP() {
+    const p = document.createElement('p');
+    return p;
+};
+
+// função setresultado
+function setResultado (msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = criaP();
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+};
